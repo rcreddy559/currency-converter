3a260bb4ced5111a61f47fd22809740b203a4f88
Ravichandra Reddy Penumuru <rc.reddy@outlook.com>
2021-04-16 15:05:51 +0200

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Currency Converter</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
